8671f60e20b8b10c465ffe407c6823b4e013c842
mikyungBae <gg5085@naver.com>
2016-07-23 12:50:37 +0900

    every form edit

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     get 'posts/index'
   # You can have the root of your site routed with "root"
   
-   root 'posts#index'
+   root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
